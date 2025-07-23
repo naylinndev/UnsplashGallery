6160ace4d2d00384a6d2a29410d6678267b867c8
Nay Linn <naylinn@weeswares.com>
2025-07-23 20:10:58 +0630

    Initial Commit

---
+++ SnapPayDemoTests/SnapPayDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapPayDemoTests.swift
+//  SnapPayDemoTests
+//
+//  Created by Nay Lin on 2025/07/23.
+//
+
+import Testing
+@testable import SnapPayDemo
+
+struct SnapPayDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
